bad468421672a284e1a15a9d4cd8bc88df11bb01
bonzzy <tomislav.fabeta@gmail.com>
2019-01-25 23:18:28 +0000

    Component PrimaryButton
    Changed the story name for buttons and updated primary-button name.

--- components/index.tsx
+++ components/index.tsx
@@ -4,8 +4,8 @@ import * as React from "react";
 import { describe, it, specs, test } from "storybook-addon-specifications";
 import {PrimaryButton} from "./buttons/primary-button";
 
-const stories = storiesOf("Button", module);
-stories.add("buttons",
+const stories = storiesOf("Buttons", module);
+stories.add("primary-button",
     () => {
         return <PrimaryButton className={"button primary-button button--stretch"}>SEARCH</PrimaryButton>;
     });
